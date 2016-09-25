1ff79f304354ca09c7f58aeeed10e505ad259fe1
Diego Castaño Chillarón <diego.castano@alumnos.upm.es>
2016-09-25 21:56:00 +0200

    Rebranding

--- website/static/website/js/users.js
+++ website/static/website/js/users.js
@@ -1,2 +1,2 @@
 var _uox = _uox || {};(function() {var s=document.createElement("script");
-s.src="https://static.usuarios-online.com/uo2.min.js";document.getElementsByTagName("head")[0].appendChild(s);})();
+    s.src="http://static.usuarios-online.com/uo2.min.js";document.getElementsByTagName("head")[0].appendChild(s);})();
